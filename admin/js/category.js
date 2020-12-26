4e608aea84fd1243cd4ebe36c7b3883ab367a0ab
Nguonchhay <nguonchhay.touch@gmail.com>
2020-12-26 15:40:06 +0700

    [TASK] Admin edit category

--- admin/js/category.js
+++ admin/js/category.js
@@ -5,9 +5,24 @@ $(document).ready(function() {
     function addItemToTable(data) {
         const tdId = '<td>' + data.id + '</td>';
         const tdName = '<td>' + data.name + '</td>';
-        const tdEdit = '<td><button class="btn btn-primary edit-category" data-id="' + data.id + '">Edit</button>';
+        const tdEdit = '<td><button class="btn btn-primary edit-category" data-id="' + data.id + '" data-name="' + data.name + '">Edit</button>';
         const tdDelete = '<button class="btn btn-danger delete-category" data-id="' + data.id + '">Delete</button></td>';
         $('#tableCategory tbody').append('<tr id="tr' + data.id + '">' + tdId + tdName + tdEdit + tdDelete + '</tr>');
+    }
+
+    // Load categories to list
+    function loadCategories() {
+        $('#tableCategory tbody').html('');
+        $.get(baseURL + '/categories', (data, status) => {
+            if (status === 'success') {
+                for (const category of data) {
+                    addItemToTable({
+                        id: category.id,
+                        name: category.name
+                    });
+                }
+            }
+        });
     }
 
     // Save category to server
@@ -30,6 +45,37 @@ $(document).ready(function() {
         });
     });
 
+    // Edit category
+    $('#frmEditCategory').submit(function(event) {
+        event.preventDefault();
+
+        const categoryElement = $('#editCategoryName');
+        const categoryId = $('#editCategoryId').val();
+        $.ajax({
+            type: "PUT",
+            url: baseURL + "/categories/" + categoryId,
+            data: {
+                name: categoryElement.val()
+            },
+            success: function() {
+                loadCategories();
+                categoryElement.val('');
+
+                // Close modal
+                $('#editCategoryModal').modal('hide');
+            }
+        });
+    });
+
+    // Edit category
+    $('#tableCategory').on('click', '.edit-category', function() {
+        const categoryId = $(this).attr('data-id');
+
+        $('#editCategoryId').val(categoryId);
+        $('#editCategoryName').val($(this).attr('data-name'));
+        $('#editCategoryModal').modal('show');
+    });
+
     // Delete category
     $('#tableCategory').on('click', '.delete-category', function() {
         if(confirm('Are you sure?')) {
@@ -45,14 +91,5 @@ $(document).ready(function() {
     });
 
     // Load categories from server
-    $.get(baseURL + '/categories', (data, status) => {
-        if (status === 'success') {
-            for (const category of data) {
-                addItemToTable({
-                    id: category.id,
-                    name: category.name
-                });
-            }
-        }
-    });
+    loadCategories();
 });
